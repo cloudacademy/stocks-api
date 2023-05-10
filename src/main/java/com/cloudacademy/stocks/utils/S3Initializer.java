72962e08d138cd4595b6f22dfbad1e0d53ad73e3
lrakai <lmrakai@gmail.com>
2023-05-10 15:49:09 -0600

    Retry initialization to dominate logs with errors

--- src/main/java/com/cloudacademy/stocks/utils/S3Initializer.java
+++ src/main/java/com/cloudacademy/stocks/utils/S3Initializer.java
@@ -1,6 +1,7 @@
 package com.cloudacademy.stocks.utils;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,8 +18,21 @@ public class S3Initializer {
     }
 
     public void init(String bucketName) {
-        if (!amazonS3.doesBucketExistV2(bucketName)) {
-            Bucket bucket = amazonS3.createBucket(bucketName);
+        while (true) {
+            try {
+                try {
+                    if (!amazonS3.doesBucketExistV2(bucketName)) {
+                        Bucket bucket = amazonS3.createBucket(bucketName);
+                        break;
+                    }
+                } catch (AmazonS3Exception e) {
+                    System.err.println("Error creating bucket: " + e.getMessage());
+                } 
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                System.err.println("Thread interrupted");
+            }
         }
     }
 }
